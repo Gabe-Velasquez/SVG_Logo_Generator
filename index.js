5db0db212d8ee5c6c88e9414e0f50caf29929bfa
Gabe <gabriel.velasquez0116@gmail.com>
2023-08-25 18:23:33 -0700

    got triangle test passing but will have to edit test for readibility purposes

---
+++ index.js
@@ -0,0 +1,44 @@
+//required modules
+const fs = require('fs');
+const inquirer = require('inquirer');
+//shape generator goes down below
+
+function validLength(input){
+    return (input.length != 3)? console.log('Keep your letters between 1 and 3 characters.'): true
+}
+
+function validInput(input){
+    return (input='')?console.log('Please enter a valid character'): true
+}
+
+//series of questions we will ask the user for their logo
+const questions = [
+    {
+        name: 'letters',
+        type: 'input',
+        message: 'Please enter three characters for your logo.',
+        validate: validInput
+    },
+    {
+        name: 'shape',
+        type: 'list',
+        message: 'Which shape would you like to choose for your logo?',
+        choices: ['Square', 'Circle', 'Triangle']
+    },
+    {
+        name:'font',
+        type:'input',
+        message: 'What color would you like your letters to be?',
+        validate: validInput
+    },
+    {
+        name: 'color',
+        type: 'input',
+        message: 'What color would you like your shape to be?',
+        validate: validInput
+    }
+];
+//passing questions through inquirer to have it generate everything when everything is collected
+function build(){
+
+}
